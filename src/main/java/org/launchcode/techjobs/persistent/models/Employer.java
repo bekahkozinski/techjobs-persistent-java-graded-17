2c3032b73862ca04f6f43f421ea1e6148a01e3d0
Rebekah Kozinski <bekahkozinski@gmail.com>
2024-11-13 18:14:58 -0600

    just after warning Task 3

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,13 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,7 +16,8 @@ public class Employer extends AbstractEntity {
     @Size(max = 100, message = "Location must be less than 100 characters")
     private String location;
 
-    public Employer() {}
+    public Employer() {
+    }
 
     public String getLocation() {
         return location;
@@ -20,4 +26,17 @@ public class Employer extends AbstractEntity {
     public void setLocation(String location) {
         this.location = location;
     }
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
+//}
